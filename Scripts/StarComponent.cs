1db36fda3cda2c8a4ee2369f5d2c860071d39943
Crazymonkay <kslambda@gmail.com>
2022-04-22 12:50:59 -0400

    Update StarComponent.cs

--- Scripts/StarComponent.cs
+++ Scripts/StarComponent.cs
@@ -5,5 +5,25 @@ namespace RoadTurtleGames.StarUtilities
     public class StarComponent : MonoBehaviour
     {
         public StarProperties StarProperties;
+        
+        private MaterialPropertyBlock materialPropertyBlock;
+        private Renderer renderer;
+
+        private void Awake()
+        {
+            materialPropertyBlock = new MaterialPropertyBlock();
+            renderer = GetComponent<Renderer>();
+        }
+
+        private void Update()
+        {
+            float temp = StarUtilities.GetNormalizedTemperature(StarProperties.Temperature);
+            float luminosity = StarUtilities.GetNormalizedEstimatedSolarLuminosityExponent(StarProperties.Temperature, StarProperties.Radius);
+
+            materialPropertyBlock.SetFloat("_StarTemperature", temp);
+            materialPropertyBlock.SetFloat("_StarLuminosity", luminosity);
+
+            renderer.SetPropertyBlock(materialPropertyBlock);
+        }
     }
 }
